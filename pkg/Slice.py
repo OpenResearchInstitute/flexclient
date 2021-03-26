e9069c95e966e955a71656406cc34b9b44df2554
Jonny <slimjm@cardiff.ac.uk>
2021-03-26 18:23:40 +0000

    Slice commands now conform to correct format

--- pkg/Slice.py
+++ pkg/Slice.py
@@ -1,11 +1,6 @@
 import itertools
 
 
-"""
-Need to find a way to reference radio outside the Main() context
-Also myRadio needs to have attributes and methods, but shouldn't be defined in Initialiser.py
-"""
-
 class Slice(object):
 	""" A Class to create, remove and alter radio frequency slices """
 	Id_iter = itertools.count() # slice_id needs to be a unique attribute for each slice 
@@ -27,7 +22,7 @@ class Slice(object):
 		
 		self.mode = mode
 
-		command = "C21|slice create"
+		command = "slice create"
 		command += (" freq=" + str(self.freq))
 		if streamID is not None:
 			self.streamID = streamID
@@ -42,7 +37,7 @@ class Slice(object):
 
 
 	def Remove(self):
-		command = "C19|slice r " + str(self.slice_id)
+		command = "slice r " + str(self.slice_id)
 		self.radio.SendCommand(command)
 		# add reply expected to reply list R|19|0|
 
@@ -57,13 +52,13 @@ class Slice(object):
 		else:
 			self.freq = freq
 
-		command = "C12|slice t " + str(self.slice_id) + " " + str(self.freq)
+		command = "slice t " + str(self.slice_id) + " " + str(self.freq)
 		self.radio.SendCommand(command)
 		#add reply expected to reply list = R12|0||
 
 
 	def Set(self, **kwargs):
-		command = "C41|slice s " + str(self.slice_id)
+		command = "slice s " + str(self.slice_id)
 		for param, arg in kwargs.items():
 			command += (" " + param + "=" + str(arg))
 
